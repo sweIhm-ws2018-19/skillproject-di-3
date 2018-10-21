7d014094b24c524911f5129fd64aa1846e1d273a
carnesirone <38287624+carnesirone@users.noreply.github.com>
2018-10-21 23:52:11 +0200

    Update Geschaeftskunde.java

--- UML/pr2Ex4/Geschaeftskunde.java
+++ UML/pr2Ex4/Geschaeftskunde.java
@@ -5,8 +5,9 @@ public class Geschaeftskunde extends Kunde {
     private String firmenname;
     private Adresse domizilAdresse;
 
-    public Geschaeftskunde(Konto[] konto, Adresse domizilAdresse) {
+    public Geschaeftskunde(Konto[] konto, String firmenname, Adresse domizilAdresse) {
         super(konto);
+        this.firmenname = firmenname;
         this.domizilAdresse = domizilAdresse;
     }
 

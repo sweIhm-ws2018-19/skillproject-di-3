8d4c1f759d9457658db8e5c8092fefb63d71332e
unknown <hahn3@hm.edu>
2018-11-23 15:19:57 +0100

    #28 sending new user story version, added new lists

--- src/main/java/verkocht/model/CookingBook.java
+++ src/main/java/verkocht/model/CookingBook.java
@@ -9,6 +9,9 @@ public class CookingBook {
 	private List<Recipe> favorites;
 	private List<Recipe> allRecipes;
 	private List<Ingredient> allIngredients;
+	private List<Recipe> meatRecipes;
+	private List<Recipe> vegetarianRecipes;
+	private List<Recipe> veganRecipes;
 	
 	public CookingBook() {
 		// Schnitzel
@@ -16,18 +19,21 @@ public class CookingBook {
 		meatRecipe.getIngredients().put(new Ingredient("Fleisch", Unit.GRAMM), 200);
 		meatRecipe.getIngredients().put(new Ingredient("Eier", Unit.STUECK), 1);
 		meatRecipe.getIngredients().put(new Ingredient("Mehl", Unit.GRAMM), 100);
+		meatRecipes.add(meatRecipe);
 		
 		// Erdbeermilchshake
 		Recipe vegetarianRecipe = new Recipe();
 		vegetarianRecipe.getIngredients().put(new Ingredient("Erdbeeren", Unit.GRAMM), 100);
 		vegetarianRecipe.getIngredients().put(new Ingredient("Milch", Unit.MILLILITER), 500);
 		vegetarianRecipe.getIngredients().put(new Ingredient("Zucker", Unit.GRAMM), 50);
+		vegetarianRecipes.add(vegetarianRecipe);
 		
 		// Nudeln mit Tomatensauce
 		Recipe veganRecipe = new Recipe();
 		veganRecipe.getIngredients().put(new Ingredient("Nudeln", Unit.GRAMM), 200);
 		veganRecipe.getIngredients().put(new Ingredient("Salz", Unit.BRISE), 1);
 		veganRecipe.getIngredients().put(new Ingredient("Tomatensauce", Unit.GRAMM), 100);
+		veganRecipes.add(veganRecipe);
 	}
 	
 	/**
@@ -44,17 +50,14 @@ public class CookingBook {
 	 * @param category
 	 * @return
 	 */
-	public Recipe findByCategory(Category category) {
+	public List<Recipe> findByCategory(Category category) {
 		switch (category) {
 		case MEAT:
-			return meatRecipe;
-			break;
+			return  meatRecipes;
 		case VEGETARIAN:
-			return vegetarianRecipe;
-			break;
+			return vegetarianRecipes;
 		case VEGAN:
-			return veganRecipe;
-			break;
+			return veganRecipes;
 		default:
 			throw new IllegalArgumentException("Unbekannte Kategorie: "+ category);
 		}

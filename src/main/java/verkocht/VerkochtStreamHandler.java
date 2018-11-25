0d6eac4b467ac059723c63c6e937bfba2fcc00e7
candydragon <jannik@laptop>
2018-11-25 23:12:28 +0100

    gitignore corrected

---
+++ src/main/java/verkocht/VerkochtStreamHandler.java
@@ -0,0 +1,64 @@
+/*
+     Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+     Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+     except in compliance with the License. A copy of the License is located at
+
+         http://aws.amazon.com/apache2.0/
+
+     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+     the specific language governing permissions and limitations under the License.
+*/
+
+package verkocht;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+
+import verkocht.handlers.CancelandStopIntentHandler;
+import verkocht.handlers.FallbackIntentHandler;
+import verkocht.handlers.HelpIntentHandler;
+import verkocht.handlers.LaunchRequestHandler;
+import verkocht.handlers.ModifyRecipeIntentHandler;
+import verkocht.handlers.SelectRecipeByCategoryIntentHandler;
+import verkocht.handlers.SelectRecipeByFavoritsIntentHandler;
+import verkocht.handlers.SelectRecipeByNameInputRecipeIntentHandler;
+import verkocht.handlers.SelectRecipeByNameStartIntentHandler;
+import verkocht.handlers.SessionEndedRequestHandler;
+import verkocht.handlers.SetFavoriteIntentHandler;
+import verkocht.handlers.TellMeCategoriesIntentHandler;
+import verkocht.handlers.TellRecipeStepsIntentHandler;
+
+public class VerkochtStreamHandler extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(
+//                        new WhatsMyColorIntentHandler(),
+//                        new MyColorIsIntentHandler(),
+                		new TellMeCategoriesIntentHandler(),
+                        new ModifyRecipeIntentHandler(),
+                        new SelectRecipeByCategoryIntentHandler(),
+                        new SelectRecipeByFavoritsIntentHandler(),
+                        new SelectRecipeByNameInputRecipeIntentHandler(),
+                        new SelectRecipeByNameStartIntentHandler(),
+//                        new SelectRecipeIntentHandler(),
+                        new SetFavoriteIntentHandler(),
+                        new TellRecipeStepsIntentHandler(),
+                        new LaunchRequestHandler(),
+                        new CancelandStopIntentHandler(),
+                        new SessionEndedRequestHandler(),
+                        new HelpIntentHandler(),
+                        new FallbackIntentHandler())
+                // Add your skill id below
+                //.withSkillId("")
+                .build();
+    }
+
+    public VerkochtStreamHandler() {
+        super(getSkill());
+    }
+
+}
---
+++ src/main/java/verkocht/model/Recipe.java
@@ -0,0 +1,54 @@
+package verkocht.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * The class that represents a recipe.
+ */
+public class Recipe {
+    private String name;
+	private List<String> steps = new ArrayList <String>();
+	private int numberOfPeople;
+	private int cookingTime;
+	private Category category;
+	private Map<Ingredient, Integer> ingredientAmounts = new HashMap<Ingredient, Integer> ();
+	
+	
+	public Recipe(String name) {
+	    this.name = name;    }
+
+    /**
+	 * 
+	 * @return
+	 */
+	public Map<Ingredient, Integer> getIngredients() {
+		return this.ingredientAmounts;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Recipe changeIngredientAmounts() {
+		return null;
+	}
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
+
+
+    
+	
+}

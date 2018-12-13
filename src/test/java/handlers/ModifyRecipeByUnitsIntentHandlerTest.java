f559d886183e2cffe2f85f4183f819d09ae05b42
carnesirone <uhrig@hm.edu>
2018-12-13 08:57:19 +0100

    #52 #30 updated and finished tests for ModifyByUnitIntent

--- src/test/java/handlers/ModifyRecipeByUnitsIntentHandlerTest.java
+++ src/test/java/handlers/ModifyRecipeByUnitsIntentHandlerTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -13,13 +15,23 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.RequestEnvelope;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
+import com.amazon.ask.model.slu.entityresolution.Resolution;
+import com.amazon.ask.model.slu.entityresolution.Resolutions;
+import com.amazon.ask.model.slu.entityresolution.Value;
+import com.amazon.ask.model.slu.entityresolution.ValueWrapper;
 import com.amazon.ask.response.ResponseBuilder;
 
 import verkocht.handlers.ModifyRecipeByUnitsIntentHandler;
 import verkocht.model.Category;
+import verkocht.model.Ingredient;
 import verkocht.model.PhrasesForAlexa;
 import verkocht.model.Recipe;
+import verkocht.model.Unit;
 
 public class ModifyRecipeByUnitsIntentHandlerTest {
 	private ModifyRecipeByUnitsIntentHandler handler;
@@ -98,42 +110,35 @@ public class ModifyRecipeByUnitsIntentHandlerTest {
                 .contains(PhrasesForAlexa.MODIFY_UNIT_NOT_DONE));
     }
     
-    /*
+    
     @Test
     public void testHandleModificationPassed() {
         Recipe.saveRecipe(testRecipe);
         testRecipe.addIngredient(new Ingredient("Ei", Unit.STUECK), 1);
-        HandlerInput inputMock = Mockito.mock(HandlerInput.class);
-        when(inputMock.getResponseBuilder()).thenReturn(new ResponseBuilder());
 
-        Optional<Response> returnResponse = handler.handle(inputMock);        
-
-        assertTrue(returnResponse.isPresent());
-        Response response = returnResponse.get();;
-
-        assertFalse(response.getShouldEndSession());
-        assertNotNull(response.getOutputSpeech());
-        assertTrue(response.getOutputSpeech().toString()
-                .contains(PhrasesForAlexa.MODIFY_UNIT_WELCOME));
-        
         final List<Slot> slotValues = new ArrayList<>();
-        Slot ingredient = Slot.builder().withName("Ingredient")
-                .withResolutions(Resolutions.builder()
-                        .addResolutionsPerAuthorityItem(Resolution.builder()
-                                .addValuesItem(ValueWrapper.builder()
-                                        .withValue(Value.builder()
-                                                .withName("Ei")
-                                                .build())
-                                        .build())
-                                .build())
-                        .build())
+        Value value = Value.builder()
+        		.withName("Ei")
+        		.build();
+        ValueWrapper valueWrapper = ValueWrapper.builder()
+        		.withValue(value)
+        		.build();
+        Resolution resolution = Resolution.builder()
+        		.addValuesItem(valueWrapper)
+        		.build();
+        Resolutions resolutions = Resolutions.builder()
+        		.addResolutionsPerAuthorityItem(resolution)
+        		.build();
+        Slot ingredient = Slot.builder()
+        		.withName("Ingredient")
+                .withResolutions(resolutions)
                 .build();
         Slot ingredientValue = Slot.builder().withName("Ingredient_Value")
                 .withValue("3")
                 .build();
         slotValues.add(ingredient);
         slotValues.add(ingredientValue);
-        final Intent.Builder intentBuilder = Intent.builder();
+        Intent.Builder intentBuilder = Intent.builder();
         slotValues.forEach((slotItem) ->
                 intentBuilder.putSlotsItem(slotItem.getName(), slotItem)
         );
@@ -143,13 +148,22 @@ public class ModifyRecipeByUnitsIntentHandlerTest {
                         .withIntent(intentBuilder.build())
                         .build())
                 .build();
-
-
+        
         HandlerInput input = Mockito.mock(HandlerInput.class);
         when(input.getResponseBuilder()).thenReturn(new ResponseBuilder());
         when(input.getRequestEnvelope()).thenReturn(requestEnvelopeMock);
 
-        returnResponse = handler.handle(inputMock);        
+        Optional<Response> returnResponse = handler.handle(input);        
+
+        assertTrue(returnResponse.isPresent());
+        Response response = returnResponse.get();;
+
+        assertFalse(response.getShouldEndSession());
+        assertNotNull(response.getOutputSpeech());
+        assertTrue(response.getOutputSpeech().toString()
+                .contains(PhrasesForAlexa.MODIFY_UNIT_WELCOME));
+        
+        returnResponse = handler.handle(input);        
 
         assertTrue(returnResponse.isPresent());
         response = returnResponse.get();;
@@ -159,5 +173,67 @@ public class ModifyRecipeByUnitsIntentHandlerTest {
         assertTrue(response.getOutputSpeech().toString()
                 .contains(PhrasesForAlexa.MODIFY_UNIT_DONE));
     }
-    */
+    
+    @Test
+    public void testHandleModificationNotPassedIngredientNotFound() {
+        Recipe.saveRecipe(testRecipe);
+        testRecipe.addIngredient(new Ingredient("Ei", Unit.STUECK), 1);
+
+        final List<Slot> slotValues = new ArrayList<>();
+        Value value = Value.builder()
+        		.withName("Milch")
+        		.build();
+        ValueWrapper valueWrapper = ValueWrapper.builder()
+        		.withValue(value)
+        		.build();
+        Resolution resolution = Resolution.builder()
+        		.addValuesItem(valueWrapper)
+        		.build();
+        Resolutions resolutions = Resolutions.builder()
+        		.addResolutionsPerAuthorityItem(resolution)
+        		.build();
+        Slot ingredient = Slot.builder()
+        		.withName("Ingredient")
+                .withResolutions(resolutions)
+                .build();
+        Slot ingredientValue = Slot.builder().withName("Ingredient_Value")
+                .withValue("3")
+                .build();
+        slotValues.add(ingredient);
+        slotValues.add(ingredientValue);
+        Intent.Builder intentBuilder = Intent.builder();
+        slotValues.forEach((slotItem) ->
+                intentBuilder.putSlotsItem(slotItem.getName(), slotItem)
+        );
+
+        final RequestEnvelope requestEnvelopeMock = RequestEnvelope.builder()
+                .withRequest(IntentRequest.builder()
+                        .withIntent(intentBuilder.build())
+                        .build())
+                .build();
+        
+        HandlerInput input = Mockito.mock(HandlerInput.class);
+        when(input.getResponseBuilder()).thenReturn(new ResponseBuilder());
+        when(input.getRequestEnvelope()).thenReturn(requestEnvelopeMock);
+
+        Optional<Response> returnResponse = handler.handle(input);        
+
+        assertTrue(returnResponse.isPresent());
+        Response response = returnResponse.get();;
+
+        assertFalse(response.getShouldEndSession());
+        assertNotNull(response.getOutputSpeech());
+        assertTrue(response.getOutputSpeech().toString()
+                .contains(PhrasesForAlexa.MODIFY_UNIT_WELCOME));
+        
+        returnResponse = handler.handle(input);        
+
+        assertTrue(returnResponse.isPresent());
+        response = returnResponse.get();;
+
+        assertFalse(response.getShouldEndSession());
+        assertNotNull(response.getOutputSpeech());
+        assertTrue(response.getOutputSpeech().toString()
+                .contains(PhrasesForAlexa.MODIFY_UNIT_NOT_DONE));
+    }
 }
